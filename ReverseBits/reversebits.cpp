e8087c7a9fb3cac060d2a424754a94b6782ac296
bigthumb <achoco@163.com>
2015-04-13 09:32:43 +0800

    Reverse bits and integer

---
+++ ReverseBits/reversebits.cpp
@@ -0,0 +1,39 @@
+/* -------------------------------------------------------------------------
+//  File Name   :   reversebits.cpp
+//  Author      :   bigthumb
+//  Create Time :   2015-4-12 23:07:08
+//  Description :  
+//
+// -----------------------------------------------------------------------*/
+
+#include <cstdint>
+
+//-------------------------------------------------------------------------
+
+class Solution
+{
+public:
+    inline uint32_t reverseBits(uint32_t n) const
+    {
+        uint32_t m = 0;
+        for(int i = 0; i < 32; ++i)
+        {
+            m <<= 1;
+            if(0 != (n & 1))
+                m |= 1;
+            n >>= 1;
+        }
+
+        return m;
+    }
+};
+
+int main(int argc, const char* argv[])
+{
+    uint32_t ret1 = Solution().reverseBits(9);
+    uint32_t ret2 = Solution().reverseBits(43261596);
+
+    return 0;
+}
+
+//--------------------------------------------------------------------------
---
+++ ReverseInteger/reverseinteger.cpp
@@ -0,0 +1,49 @@
+/* -------------------------------------------------------------------------
+//  File Name   :   reverseinteger.cpp
+//  Author      :   bigthumb
+//  Create Time :   2015-4-12 23:22:22
+//  Description :  
+//
+// -----------------------------------------------------------------------*/
+
+#include <cstdint>
+#include <limits>
+
+//-------------------------------------------------------------------------
+
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        long long res = 0L;
+        for(; 0 != x; x /= 10)
+        {
+            res *= 10;
+            res += x % 10;
+        }
+
+        if(res < std::numeric_limits<int>().min() || res > std::numeric_limits<int>().max())
+            return 0;
+        else
+            return static_cast<int>(res);
+    }
+};
+
+int main(int argc, const char* argv[])
+{
+    int ret1 = Solution().reverse(0);
+    int ret2 = Solution().reverse(123);
+    int ret3 = Solution().reverse(-123);
+
+    int ret4 = Solution().reverse(-2147483648);
+    int ret5 = Solution().reverse(2147483647);
+    int ret6 = Solution().reverse(1007483004);
+
+    int ret7 = Solution().reverse(-12040);
+    int ret8 = Solution().reverse(200100);
+
+    return 0;
+}
+
+//--------------------------------------------------------------------------
